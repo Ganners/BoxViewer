7042e8ca6fee8e436610057b6212134e4fc1e387
Mark Gannaway <mark@ganners.co.uk>
2014-11-08 11:43:58 +0000

    Updated tests to include file storage location

--- boxapi/boxapi_test.go
+++ boxapi/boxapi_test.go
@@ -3,18 +3,22 @@ package boxapi
 import "testing"
 
 const key string = "My Super Unique Key"
+const location string = "/var/www/location"
 
-func TestKeyIsSet(t *testing.T) {
+func TestKeyAndLocationIsSet(t *testing.T) {
 
-	box := NewBoxApi(key)
+	box := NewBoxApi(key, location)
 	if box.ApiKey != key {
 		t.Errorf("Key not set error got %s, want %s", box.ApiKey, key)
+	}
+	if box.FileLocation != location {
+		t.Errorf("Location not set error got %s, want %s", box.FileLocation, location)
 	}
 }
 
 func TestCanGenerateUniqueFilename(t *testing.T) {
 
-	box := NewBoxApi(key)
+	box := NewBoxApi(key, location)
 	filePath := "http://www.mmta.co.uk/newsletter/crucible"
 	expected := "6588e5fba5a3636ba6249eb72246f616"
 
