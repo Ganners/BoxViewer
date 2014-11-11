d58f41346774c9ad47ab757d1a61ff868e508d2b
Mark Gannaway <mark@cyb.co.uk>
2014-11-11 11:22:13 +0000

    Updated build script to execute properly

--- scripts/build/build-production.sh
+++ scripts/build/build-production.sh
@@ -7,5 +7,5 @@ cd src/boxviewer/
 /usr/local/go/bin/go test
 /usr/local/go/bin/go install
 
-killall -9 /var/www/apps/boxviewer/bin/boxviewer
-/var/www/apps/boxviewer/bin/boxviewer --key="rnii4dbpu16254m844ikzx4jxi2u8sj4" --location="files"
+killall -9 boxviewer
+nohup /var/www/apps/boxviewer/bin/boxviewer --key="rnii4dbpu16254m844ikzx4jxi2u8sj4" --location="/var/www/apps/boxviewer_files/" &
