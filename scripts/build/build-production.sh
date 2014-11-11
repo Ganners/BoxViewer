7618ab7eca60a6f9fd7761eeb8fcd471f0c3d648
Mark Gannaway <mark@cyb.co.uk>
2014-11-11 11:11:29 +0000

    Fixed path error

--- scripts/build/build-production.sh
+++ scripts/build/build-production.sh
@@ -4,8 +4,8 @@ export GOPATH=$PWD
 export GOBIN=$GOPATH/bin
 
 cd src/boxviewer/
-/usr/local/go/bin test
-/usr/local/go/bin install
+/usr/local/go/bin/go test
+/usr/local/go/bin/go install
 
 killall -9 /var/www/apps/boxviewer/bin/boxviewer
 /var/www/apps/boxviewer/bin/boxviewer --key="rnii4dbpu16254m844ikzx4jxi2u8sj4" --location="files"
